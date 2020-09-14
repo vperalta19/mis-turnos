d3c1477909ce8fb4d4b26e944054f664ef9fc4fb
Mateo <maferretto@oadearl.com.ar>
2020-09-14 10:13:01 -0300

    tabssss

--- src/components/Novedad.js
+++ src/components/Novedad.js
@@ -7,32 +7,32 @@ import Typography from '@material-ui/core/Typography';
 import logo from './../assets/img/logo.png'
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
+	root: {
+		maxWidth: 345,
+	},
+	media: {
+		height: 140,
+	},
 });
 
 export default function Novedad() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <Card className='card' >
-        <CardMedia
-          className={classes.media}
-          image={logo}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-    </Card>
-  );
+	return (
+		<Card className='card' >
+			<CardMedia
+				className={classes.media}
+				image={logo}
+			/>
+			<CardContent>
+				<Typography gutterBottom variant="h5" component="h2">
+					Lizard
+				</Typography>
+				<Typography variant="body2" color="textSecondary" component="p">
+					Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+					across all continents except Antarctica
+				</Typography>
+			</CardContent>
+		</Card>
+	);
 }
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -8,8 +8,8 @@ import logo from './../assets/img/logo.png'
 function openNav() {
 	document.getElementById("mySidebar").style.width = "250px";
 	document.getElementById("main").style.marginLeft = "250px";
-  }
-  
+}
+
 function closeNav() {
 	document.getElementById("mySidebar").style.width = "0";
 	document.getElementById("main").style.marginLeft= "0";
