ad75fe016c499ecdd4d4e257db3f48d80a5730db
machi918 <mati_d_pinto@hotmail.com>
2020-09-14 19:13:24 -0300

    Cambios en inicio de sesion

--- src/pages/InicioSesion.js
+++ src/pages/InicioSesion.js
@@ -2,23 +2,48 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../assets/css/InicioSesion.css'
 
-export default function InicioSesion() {
+/*export default function InicioSesion() {
 	return (
         <div className="App">
-            <div className="main-login-signup">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-lg-4">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-lg-12">
+                        <div className="test">
+                        PRUEBA
                         </div>
-                        <div className="col-lg-4">
-                            <div className="login">
-                                asdasdasd
-                            </div>
-                        </div>
-                        <div className="col-lg-4">
+                    </div>
+                    <div className="col-lg-12">
+                        <div className="login">
+                            asdasdasd
                         </div>
                     </div>
                 </div>       
+            </div> 
+        </div>
+	);
+}*/
+
+export default function InicioSesion() {
+	return (
+        <div className="App">
+            <div className = "container">
+                <div className="row h-100 align-items-center justify-content-center">
+                    <div className="col-lg-6 col-12">
+                        <div className="main-login">   
+                            <div className="test">
+                                PRUEBA
+                            </div> 
+                            <div className="login">
+                                asdasdasd
+                            </div>        
+                        </div>
+                        <div className="main-signup">
+                            <div className="signup">
+                                ¿Primera vez aqui?
+                            </div>
+                        </div>       
+                    </div>
+                </div>
             </div>
         </div>
 	);
