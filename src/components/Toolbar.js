fe0be291e51d05e6e2e46e94d2069c0560523ee1
vperalta19 <56236510+vperalta19@users.noreply.github.com>
2020-09-14 15:32:48 -0300

    pruebas

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -16,6 +16,29 @@ function closeNav() {
 }
 
 
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyHeader() {
+	// Get the navbar
+	var navbar = document.getElementById("main-header");
+	var pedirTurno = document.getElementById("pedirTurno");
+
+	// Get the offset position of the navbar
+	var sticky = navbar.offsetTop;
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky"); 
+        pedirTurno.classList.add("d-none");
+        pedirTurno.classList.add("d-lg-block");
+        
+    } else {
+        navbar.classList.remove("sticky");
+        pedirTurno.classList.remove("d-none");
+    }
+}
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {stickyHeader()};
+
+
 
 export default function Toolbar(props) {
 	let name = props.toolbarInfo.nombre;
@@ -126,116 +149,5 @@ export default function Toolbar(props) {
 	);
 }
 
-
-
-
-//HTML
-
-/*<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/fontawesome-free-5.14.0-web/css/all.css">
-	<link rel="stylesheet" href="css/styles.css">
-	<title>Document</title>
-</head>
-<body>
-	<header>
-		<div className="header-area">
-			<div  className="header-top_area d-none d-lg-block">
-				<div className="container">
-					<div className="row">
-						<div className="col">
-							<div className="usuario">
-								<div>{name}</div>
-							</div>
-						</div>
-						<div className="col">
-							<div className="cerrarSesion">
-								<div>CERRAR SESION</div>
-							</div>
-							
-						</div>
-					</div>
-				</div>
-			</div>
-			<div id="main-header" className="main-header-area">
-				<div className="container">
-					<div className="row align-items-center">
-						<div className="col-lg-3 col-4">
-							<div>
-								<img src="css/img/logo.png" alt="logo">
-							</div>
-						</div>
-						<div className="col-lg-6 d-none d-lg-block text-center">
-							<div className="toolbar">
-								<ul>
-									<li>
-										<div>INICIO</div>
-									</li>
-									<li>
-										<div>CALENDARIO</div>
-									</li>
-									<li>
-										<div>MI CUENTA</div>
-									</li>
-								</ul>
-							</div>
-						</div>
-
-						<div className="col-8 d-lg-none" >
-							<div id="mySidebar" className="sidebar">
-								<a href="#" onclick="closeNav()"><i className="fas fa-times-circle"></i></div>
-								<a href="#" style="font-size: 15px;">Juan Manuel Belgrano</div>
-								<div>INICIO</div>
-								<div>CALENDARIO</div>
-								<div>MI CUENTA</div>
-								<div>CERRAR SESION</div>
-							  </div>
-							  
-							  <div id="main">
-								<a onclick="openNav()">☰</div> 
-							  </div>
-							
-						</div>
-						<div className="col-lg-3 col-12" id="pedirTurno">
-							<div className="pedirTurno">
-								<div>PEDIR TURNO</div>
-							</div>
-						</div>
-
-				</div>
-			</div>
-		</div>
-	</header>
-	<script type="text/javascript" src="js/navbar.js"></script>
-</body>
-</html> */
-
-//JS
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyHeader() {
-	// Get the navbar
-	var navbar = document.getElementById("main-header");
-	var pedirTurno = document.getElementById("pedirTurno");
-
-	// Get the offset position of the navbar
-	var sticky = navbar.offsetTop;
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky"); 
-        pedirTurno.classList.add("d-none");
-        pedirTurno.classList.add("d-lg-block");
-        
-    } else {
-        navbar.classList.remove("sticky");
-        pedirTurno.classList.remove("d-none");
-    }
-}
-
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {stickyHeader()};
 
 
