9abe72e28dd235ee24d2122ca853512dad41eb98
vperalta19 <56236510+vperalta19@users.noreply.github.com>
2020-09-16 14:04:18 -0300

    landing hecha

--- src/components/DondeEstamos.js
+++ src/components/DondeEstamos.js
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
+
 import React from 'react';
 import './../assets/css/components.css'
 
@@ -11,7 +12,7 @@ export default function DondeEstamos() {
                     <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an.</span>
                 </div>
                 <div className="col-12 col-md-6 col-lg-8">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.406878843279!2d-58.406567984801434!3d-34.593871480462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca9a9f0e8cb9%3A0x95fc444776ee9d31!2sAv.%20Santa%20Fe%202644%2C%20C1425%20BGO%2C%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1600259097269!5m2!1ses-419!2sar" width="100%" height="50%" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.406878843279!2d-58.406567984801434!3d-34.593871480462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca9a9f0e8cb9%3A0x95fc444776ee9d31!2sAv.%20Santa%20Fe%202644%2C%20C1425%20BGO%2C%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1600275305731!5m2!1ses-419!2sar" width="100%" height="50%" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
         </div>
