5b1a869d79cd1f97f232934ad39657bd4b044b5a
machi918 <mati_d_pinto@hotmail.com>
2020-09-18 16:15:33 -0300

    Unicamente falta los forms y los botones

--- src/pages/InicioSesion.js
+++ src/pages/InicioSesion.js
@@ -5,46 +5,24 @@ import './../assets/css/InicioSesion.css';
 
 function InicioSesion() {
 	return (
-        <div className ="fondoLogSign">
-            <div className="header">
-                <div className = "container-fluid contenedor">
-                    <div className="row fil1">
-                        <div className = "col-lg-4 col-md-3 col-1"> 
-                        </div>
-                        <div className="col-lg-4 col-10 col-md-6 login">
-                            <h1 id="titleLogIn">Iniciar Sesion</h1>
-                        </div>
+        <div className="fondoLogSign">
+            <div className = "container-fluid contenedor">
+                <div className="row fil1">
+                    <div className = "col-lg-4 col-md-3 col-1"> 
                     </div>
-                    <div className="row fil2">
-                        <div className = "col-lg-4 col-md-3 col-1">
-                        </div>
-                        <div className="col-lg-4 col-10 col-md-6 signup">
-                            <h1 id="titleSignup">¿Primera vez aquí?</h1>
-                        </div>
+                    <div className="col-lg-4 col-10 col-md-6 login">
+                        <h1 id="titleLogIn">Iniciar Sesion</h1>
+                    </div>
+                </div>
+                <div className="row fil2">
+                    <div className = "col-lg-4 col-md-3 col-1">
+                    </div>
+                    <div className="col-lg-4 col-10 col-md-6 signup">
+                        <h1 id="titleSignup">¿Primera vez aquí?</h1>
                     </div>
                 </div>
             </div>
         </div>
-
-        // <div className ="fondoLogSign">
-        //     <div className = "container-fluid contenedor">
-        //         <div className="row fil1">
-        //             <div className = "col-lg-4 col-md-3 col-1"> 
-        //             </div>
-        //             <div className="col-lg-4 col-10 col-md-6 login">
-        //                 <h1 id="titleLogIn">Iniciar Sesion</h1>
-        //             </div>
-        //         </div>
-        //         <div className="row fil2">
-        //             <div className = "col-lg-4 col-md-3 col-1">
-        //             </div>
-        //             <div className="col-lg-4 col-10 col-md-6 signup">
-        //                 <h1 id="titleSignup">¿Primera vez aquí?</h1>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-
 
             // <div className="container-fluid fondoLogSign">
             //     <div className="row">
