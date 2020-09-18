8be0575245e8e258c979a80dfed3c656282b19a4
machi918 <mati_d_pinto@hotmail.com>
2020-09-18 16:21:26 -0300

    Unicamente falta los forms y los botones

--- src/pages/InicioSesion.js
+++ src/pages/InicioSesion.js
@@ -23,23 +23,6 @@ function InicioSesion() {
                 </div>
             </div>
         </div>
-
-            // <div className="container-fluid fondoLogSign">
-            //     <div className="row">
-            //     </div>
-            //     <div className="row fil1">
-            //         <div className="col-lg-4 col-md-3">
-            //         </div>
-            //         <div className="col-lg-4 col-12 col-md-6 login">
-            //         </div>
-            //     </div>
-            //     <div className="row">
-            //         <div className="col-lg-4 col-md-3">
-            //         </div>
-            //         <div className="col-lg-4 col-12 col-md-6 signup">
-            //         </div>
-            //     </div>  
-            // </div> 
 	);
 }
 export default InicioSesion
