4a80761f7b9d68d6f4b4f9c7a902203353024b8f
machi918 <mati_d_pinto@hotmail.com>
2020-09-21 18:35:06 -0300

    En desarrollo - Inicio de Sesion y Registro

--- src/pages/InicioSesion.js
+++ src/pages/InicioSesion.js
@@ -7,12 +7,13 @@ function InicioSesion() {
 	return (
         <div className="fondoLogSign">
             <div className = "container-fluid contenedor">
-                <div className="row fil1">
-                    <div className = "col-lg-4 col-md-3 col-1"> 
+                <div className="row fil1 align-items-center">
+                    <div className = "col-lg-3 col-md-3 col-1 col-xl-4"> 
                     </div>
-                    <div className="col-lg-4 col-10 col-md-6 login">
+                    <div className="col-lg-6 col-10 col-md-6 col-xl-4 login">
                         <h1 className="titleLogIn">Iniciar Sesion</h1>
                         <form id="inputLogin" className="inputLogin">
+                            <span className="doclabel">Documento</span><br></br>
                             <input type="text" id="docid" name="docid" className="docid"></input>
                             <input type="password" id="pass" name="pass" className="pass"></input>
                             <input type="submit" value="Iniciar Sesion" id="sub" className="sub"></input>
--- src/pages/Registrar.js
+++ src/pages/Registrar.js
@@ -5,28 +5,75 @@ import './../assets/css/Registrar.css';
 
 function Registrar() {
 	return (
+        // <div className="fondoRegistrar">
+        //     <div className="container contenedor">
+        //         <div className="row fil1">
+        //             <div className = "col-lg-4 col-md-3 col-1"> 
+        //             </div>
+        //             <div className="col-lg-4 col-10 col-md-6 registro_cont">
+        //                 <div className="row fil1_r">
+        //                     <div className="col-12 forms_fill">
+        //                         <form>
+        //                             <span className="namelabel">Nombre</span><br></br>
+        //                             <input type="text" id="name" name="name" className="name"></input>
+        //                             <span className="surnamelabel">Apellido</span><br></br>
+        //                             <input type="text" id="surname" name="surname" className="surname"></input>
+        //                             <span className="docidlabel">Documento</span><br></br>
+        //                             <input type="text" id="docid" name="docid" className="docid"></input>
+        //                             <span className="emaillabel">Email</span><br></br>
+        //                             <input type="text" id="email" name="email" className="email"></input>
+        //                             <span className="nacimientolabel">Fecha de nacimiento</span><br></br>
+        //                             <input type="text" id="nacimiento" name="nacimiento" className="nacimiento"></input>
+        //                             <span className="obsociallabel">Obra social</span><br></br>
+        //                             <input type="text" id="obsocial" name="obsocial" className="obsocial"></input>
+        //                             <span className="nsociolabel">Numero de socio</span><br></br>
+        //                             <input type="text" id="nsocio" name="nsocio" className="nsocio"></input>
+        //                             <input type="submit" value="Registrarse" id="sub" className="sub text-center"></input>
+        //                         </form>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
         <div className="fondoRegistrar">
             <div className="container contenedor">
                 <div className="row fil1">
                     <div className = "col-lg-4 col-md-3 col-1"> 
                     </div>
                     <div className="col-lg-4 col-10 col-md-6 registro_cont">
-                        <div className="container forms_cont">
-                            <div className="row fil1_r">
-                                <div className="col-1">
-                                </div>
-                                <div className="col-12 forms_fill">
-                                    <form>
-                                        <input type="text" id="name" name="name"></input>
-                                        <input type="text" id="surname" name="surname"></input>
-                                        <input type="text" id="docid" name="docid"></input>
-                                        <input type="text" id="email" name="email"></input>
-                                        <input type="text" id="nacimiento" name="nacimiento"></input>
-                                        <input type="text" id="obsocial" name="obsocial"></input>
-                                        <input type="text" id="nsocio" name="nsocio"></input>
-                                        <input type="submit" value="Registrarse" id="sub"></input>
-                                    </form>
-                                </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <span className="namelabel">Nombre</span><br></br>
+                                <input type="text" id="name" name="name" className="name"></input>
+                            </div>
+                            <div className="col-12">
+                                <span className="surnamelabel">Apellido</span><br></br>
+                                <input type="text" id="surname" name="surname" className="surname"></input>
+                            </div>
+                            <div className="col-12">
+                                <span className="docidlabel">Documento</span><br></br>
+                                <input type="text" id="docid" name="docid" className="docid"></input>
+                            </div>
+                            <div className="col-12">
+                                <span className="emaillabel">Email</span><br></br>
+                                <input type="text" id="email" name="email" className="email"></input>
+                            </div>
+                            <div className="col-12">
+                                <span className="nacimientolabel">Fecha de nacimiento</span><br></br>
+                                <input type="text" id="nacimiento" name="nacimiento" className="nacimiento"></input>
+                            </div>
+                            <div className="col-12">
+                                <span className="obsociallabel">Obra social</span><br></br>
+                                <input type="number" id="obsocial" name="obsocial" className="obsocial"></input>
+                            </div>
+                            <div className="col-12">
+                                <span className="nsociolabel">Numero de socio</span><br></br>
+                                <input type="text" id="nsocio" name="nsocio" className="nsocio"></input>
+                            </div>
+                            <div className="col-12 text-center">
+                                <input type="button" value="Registrarse" id="sub" className="sub text-center"></input>
                             </div>
                         </div>
                     </div>
