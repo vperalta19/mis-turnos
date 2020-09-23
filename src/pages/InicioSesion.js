d30c38daefc248a95bb2c44dbe34986d9c49e4f9
machi918 <mati_d_pinto@hotmail.com>
2020-09-23 09:05:40 -0300

    Finalizado - Inicio de Sesion y Registrar Paciente

--- src/pages/InicioSesion.js
+++ src/pages/InicioSesion.js
@@ -54,16 +54,16 @@ function InicioSesion() {
                         <div className="col text-center">
                             <input type="password" id="pass" name="pass" className="pass"></input>
                         </div>
-                        <div className="col">
+                        <div className="col text-center">
                             <button type="button" className="recovery">Olvidé mi contraseña</button> 
-                            <input type="submit" value="Iniciar Sesion" id="sub" className="sub align-items-right"></input>
+                            <input type="submit" value="Iniciar Sesion" id="sub" className="sub"></input>
                         </div>
                     </div>
                 </div>
                 <div className="row fil2">
-                    <div className = "col-lg-4 col-md-3 col-1">
+                    <div className = "col-lg-3 col-md-3 col-1 col-xl-4">
                     </div>
-                    <div className="col-lg-4 col-10 col-md-6 signup">
+                    <div className="col-lg-6 col-10 col-md-6 col-xl-4 signup">
                         <h1 id="titleSignup" className="titleSignup">¿Primera vez aquí?</h1>
                         <p id="textSignup" className="textSignup">Regístrate y empieza a utilizarlo</p>
                         <button type="button" className="buttonSignUp">Registrar</button>
