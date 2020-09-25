362a5eed9fa7c9b360b4e718585efef0b1a3e808
vperalta19 <56236510+vperalta19@users.noreply.github.com>
2020-09-25 11:40:15 -0300

    por las dudas

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import MiCuenta from './pages/MiCuenta';
 import Calendario from './pages/Calendario';
 import PedirTurno from './pages/PedirTurno';
 import Inicio from './pages/Inicio';
-import InicioSesion from './pages/InicioSesion';
-import Registrar from './pages/Registrar';
 
 export default function App() {
 	const toolbarInfo={
@@ -18,12 +16,10 @@ export default function App() {
 				<div className="App">
 					<Toolbar toolbarInfo={toolbarInfo}/>
 					<Switch>
-						<Route path='/' component={Inicio} />
+						<Route path='/' exact component={Inicio} />
 						<Route path='/Calendario' component={Calendario} />
 						<Route path='/MiCuenta' component={MiCuenta} />
 						<Route path='/PedirTurno' component={PedirTurno} />
-						<Route path='/InicioSesion' component={InicioSesion} />
-						<Route path='/Registrar' component={Registrar} />
 					</Switch>
 				</div>
 			</Router>
