1500b6b6d3f56c92fac59dda91f2529689244c79
machi918 <mati_d_pinto@hotmail.com>
2020-10-24 19:56:18 -0300

    Creacion de estructura del back

---
+++ mis-turnosBack/index.js
@@ -0,0 +1,31 @@
+//Imports
+const express = require('express');
+const app = express();
+//------------------------------------------------------------------------------------------------------------
+
+//Creo port para indicar el puerto en el que va a funcionar el servidor
+const port = 3500;
+//------------------------------------------------------------------------------------------------------------
+
+//Middlewares
+app.use(express.json());
+//------------------------------------------------------------------------------------------------------------
+
+//Routes
+// app.use(require('./routes/Pacientes'));
+// app.use(require('./routes/Profesionales'));
+// app.use(require('./routes/Turnos'));
+// app.use(require('./routes/Recetas'));
+// app.use(require('./routes/franjaHoraria'));
+// app.use(require('./routes/Medicamentos'));
+// app.use(require('./routes/Estudios'));
+// app.use(require('./routes/Novedades'));
+
+//------------------------------------------------------------------------------------------------------------
+
+//Levantando el server con express
+app.listen(port,function(err){
+    if(err) throw err;
+    console.log('App escuchando en http://localhost:'+port);
+});
+//------------------------------------------------------------------------------------------------------------
