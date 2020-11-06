12d2ea5fbced0456233dac61e48364ca5d1e3b5a
vperalta19 <56236510+vperalta19@users.noreply.github.com>
2020-11-06 10:17:34 -0300

    login terminado

--- mis-turnosBack/index.js
+++ mis-turnosBack/index.js
@@ -16,8 +16,7 @@ app.use(express.json());
 //------------------------------------------------------------------------------------------------------------
 
 //Routes
-app.use(require('./routes/Pacientes'));
-app.use(require('./routes/Profesionales'));
+app.use(require('./routes/Usuarios'));
 // app.use(require('./routes/Turnos'));
 // app.use(require('./routes/Recetas'));
 // app.use(require('./routes/franjaHoraria'));
