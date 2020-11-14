3d24a0827e6a682281a967b277a1a97fdfc52365
machi918 <mati_d_pinto@hotmail.com>
2020-11-14 19:49:01 -0300

    Agregando soporte de mail

--- mis-turnosBack/routes/Turnos.js
+++ mis-turnosBack/routes/Turnos.js
@@ -5,32 +5,35 @@ const router = express.Router();
 
 const mysqlConnection = require('../Database');
 
+// import {mailSend} from '../services/mailer';
+const mails = require('../services/mailer');
+
 //------------------------------------------------------------------------------------------------------------
 
-function mail(correoDireccion, htmlContenido, subject){
-    let transporter = nodemailer.createTransport({
-        service: 'hotmail',
-        auth: {
-        user: '-',
-        pass: '-'
-        }
-    });
+// function mail(correoDireccion, htmlContenido, subject){
+//     let transporter = nodemailer.createTransport({
+//         service: 'hotmail',
+//         auth: {
+//         user: '-',
+//         pass: '-'
+//         }
+//     });
 
-    let mailOptions = {
-    from: 'vaperalta@uade.edu.ar',
-    to: correoDireccion,
-    subject: subject,
-    html: htmlContenido
-    };
+//     let mailOptions = {
+//     from: 'vaperalta@uade.edu.ar',
+//     to: correoDireccion,
+//     subject: subject,
+//     html: htmlContenido
+//     };
 
-    transporter.sendMail(mailOptions, function(error, info) {
-		if (error) {
-			console.log(error);
-		} else {
-			console.log('Email sent: ' + info.response);
-		}
-    });
-}
+//     transporter.sendMail(mailOptions, function(error, info) {
+// 		if (error) {
+// 			console.log(error);
+// 		} else {
+// 			console.log('Email sent: ' + info.response);
+// 		}
+//     });
+// }
 
 router.get('/getTurnos', function(req, res) {
 	mysqlConnection.query('SELECT idTurno, fechaInicio, fechaFin, nombre, apellido, dni FROM Turnos T, Usuarios U WHERE T.paciente = U.dni' , function(err, result){
@@ -63,13 +66,17 @@ router.post('/crearTurno', function(req, res) {
 			mysqlConnection.query(
 				"SELECT email FROM Usuarios WHERE dni = " +  req.body.dni,
 				(err, row, field) => {
-					mail(row[0].email, "Tienes un nuevo turno el día " + req.body.inicio.substring(0, 11), "Nuevo Turno");
+					// mail(row[0].email, "Tienes un nuevo turno el día " + req.body.inicio.substring(0, 11), "Nuevo Turno");
+					let cuerpo = "<body>Tienes un nuevo turno el día " +req.body.inicio.substring(0, 11)+ "</body>";
+					mails.mailSend("¡Nuevo turno agregado! - Mis-turnos.com",row[0].email,cuerpo);
 				}
 			)
 			mysqlConnection.query(
 				'SELECT email FROM Usuarios WHERE rol = "medico"',
 				(err, row, field) => {
-					mail(row[0].email, "Tienes un nuevo turno el día " + req.body.inicio.substring(0, 11), "Nuevo Turno");
+					// mail(row[0].email, "Tienes un nuevo turno el día " + req.body.inicio.substring(0, 11), "Nuevo Turno");
+					let cuerpo = "<body>Tienes un nuevo turno el día " +req.body.inicio.substring(0, 11)+ "</body>";
+					mails.mailSend("¡Nuevo turno agregado! - Mis-turnos.com",row[0].email,cuerpo);
 				}
 			)
 		}
